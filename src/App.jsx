36280169989b2188feba1b820820fb36cadc5767
Giovanni Coakley <gcoakley.jr@gmail.com>
2022-10-17 22:37:06 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { useSelector } from "react-redux";
+
+const App = () => {
+  const user = useSelector(state => state.user.currentUser)
+  return (
+    <Routes>
+      <Route path='/' exact element={<Home />} />
+      <Route path='/product/:id' element={<Product />} />
+      <Route path='/products/:category' element={<ProductList />} />
+      <Route path='/register' element={user ? <Navigate to="/" /> : <Register />} />
+      <Route path='/login' element={user ? <Navigate to="/" /> : <Login />} />
+      <Route path='/cart' element={<Cart />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import App from './App';
+import { Provider } from "react-redux"
+import { store, persistor } from "./redux/store"
+import { PersistGate } from 'redux-persist/integration/react'
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(<BrowserRouter>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App tab="home" />
+        </PersistGate>
+    </Provider>
+</BrowserRouter>)
